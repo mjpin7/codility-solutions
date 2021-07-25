1c54d71b4db360652db9b40d6aa1c8e895741568
Marcel Pinheiro <32338815+mjpin7@users.noreply.github.com>
2021-07-25 14:47:50 +0000

    Added binary gap

---
+++ BinaryGap/binarygap.go
@@ -0,0 +1,30 @@
+package solution
+
+import (
+    "strconv"
+    "strings"
+)
+
+
+func Solution(N int) int {
+    n := strconv.FormatInt(int64(N), 2)
+    num := 0
+
+	// Not sure I like this "infinite loop"
+    for {
+		// Get index of first and second "1" in string
+        i := strings.Index(n, "1")
+        j := strings.Index(n[i+1:], "1")
+
+        if j == -1 {
+            break;
+        }
+
+        if j - i > num {
+            num = j - i
+        }
+        n = n[j+1:]
+    }
+
+    return num
+}
