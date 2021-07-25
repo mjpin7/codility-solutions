7adbb00dabc8a3939e9e7f4b538b5dd22d674417
Marcel Pinheiro <32338815+mjpin7@users.noreply.github.com>
2021-07-25 21:01:25 +0000

    Added permmissingelem

---
+++ PermMissingElem/package.go
@@ -0,0 +1,25 @@
+package solution
+
+import "sort"
+
+func Solution(A []int) int {
+    sort.Ints(A)
+    var num int
+
+    if len(A) == 0 {
+        num = 1
+    } else {
+        if A[0] != 1 {
+            num = 1
+        } else {
+            for i := 0; i < len(A); i++ {
+                if i == len(A) - 1 || A[i+1] - A[i] > 1 {
+                    num = A[i] + 1
+                    break;
+                }
+            }
+        }
+    }
+
+    return num
+}
