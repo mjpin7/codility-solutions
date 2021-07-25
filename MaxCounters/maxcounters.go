0652529ef3d50fa923e3dce0ec5e9c1ce0c5df59
Marcel Pinheiro <32338815+mjpin7@users.noreply.github.com>
2021-07-25 15:17:31 +0000

    Added max counters

---
+++ MaxCounters/maxcounters.go
@@ -0,0 +1,22 @@
+package solution
+
+func Solution(N int, A []int) []int {
+    result := make([]int, N)
+    var max int
+
+    for _, v := range A {
+        if 1 <= v && v <= N {
+            result[v-1]++
+            if result[v-1] > max {
+                max = result[v-1]
+            }
+        } else if v == N + 1 {
+            for i := range result {
+                result[i] = max
+            }
+        }
+    }
+
+    return result
+}
+
